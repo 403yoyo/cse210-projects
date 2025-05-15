84ae3be7fafd8eddaa1800cfd6e1958cd0c68f2e
yoyo <etiuzalethankgod@gmail.com>
2025-05-15 16:05:00 +0100

    done with week1 assignment

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,55 @@
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+//     }
+// }
+
 using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter;
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Here is your grade: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You did great.");
+        }
+        else
+        {
+            Console.WriteLine("Keep working hard, You will do better next time.");
+        }
     }
 }
