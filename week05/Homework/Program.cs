d08b1b94b188099ebaad5be1e413dee9485e8ece
yoyo <etiuzalethankgod@gmail.com>
2025-06-06 16:14:46 +0100

    done with week5

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assignment = new Assignment("Chukwuemeka Ohenhen", "Addition");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment mathAssignment = new MathAssignment(
+            "Aisha Okoro", "Fractions", "7.3", "8-19");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment writingAssignment = new WritingAssignment(
+            "Emeka Nwachukwu", "Chinese History", "How the great wall came about");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
     }
 }
