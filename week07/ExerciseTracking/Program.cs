c9cbec973bf8917bd2189825e7a06b99451f5eef
Etiuzale ThankGod Ikpemhinoghena <etiuzalethankgod@gmail.com>
2025-06-17 11:00:20 +0100

    done with Exercise Tracking Program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,33 @@
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+//     }
+// }
+
+
+
+
+
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running("14 Jun 2025", 30, 4.8));
+        activities.Add(new Cycling("15 Jun 2025", 45, 20.0));
+        activities.Add(new Swimming("16 Jun 2025", 40, 30));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
